5d582bc42f2835a9d2eebd7a40fbd36878ccc782
jonjj2016 <jonmartirosyan1988@gmail.com>
2021-02-20 18:21:51 +0800

    created Product Homescreen and routing

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../Components/Product';
+import products from '../products';
+
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+            {
+              products.map((item) => (
+                <Col key={ item._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product   {...item}/>
+                </Col>
+              ))
+            }
+         </Row>
+    </>
+  )
+};
+
+export default HomeScreen
